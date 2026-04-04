a2c8b18b581de2de21eb5d5cd86c07c0c2ef7289
AKRiLLiCK <akrillick@proton.me>
2026-04-04 14:48:44 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,5 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-    session: true,
-  }),
+  adapter: cloudflare(),
 });
