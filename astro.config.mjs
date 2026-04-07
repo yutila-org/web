8f7fe9436e8971777ea041601989ececb0b9b002
AKRiLLiCK <akrillick@proton.me>
2026-04-07 16:55:13 +0200

    Updated roles and updated astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
-  session: {
-    driver: 'cloudflare-kv-binding',
-    cookie: {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
-      maxAge: 86400,
-    },
-  },
+    output: 'server',
+    adapter: cloudflare({
+        sessionKVBindingName: 'SESSIONS'
+    }),
+    session: {
+        cookie: {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            maxAge: 86400
+        }
+    }
 });
